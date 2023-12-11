4dfd8fdc0f845dadd0fb5b74f3f99bbe35bddcca
dapperAuteur <a@awews.com>
2023-12-10 19:28:54 -0700

    add financial account mutations that don't work

---
+++ app/api/graphql/resolvers/mutations/financianlAcccountMutations.js
@@ -0,0 +1,12 @@
+import clientPromise from "./../../../../../lib/mongodb";
+import { ObjectId } from 'mongodb';
+import shuffle from "shuffle-array";
+
+const dbCollection = "FinancialAccount";
+const dbName = "palabaras-express-api";
+
+export const FinancialAccountMutations = {
+  createFinancialAccount: async (_, args, context) => {
+    
+  }
+}
