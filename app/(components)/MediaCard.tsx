e195b9364cba2004434fa21840f6c1e8ce5df68e
awe <aweful@awews.com>
2023-12-11 16:54:25 -0700

    comment out link and props for link to resolve typescript error

--- app/(components)/MediaCard.tsx
+++ app/(components)/MediaCard.tsx
@@ -11,21 +11,21 @@ const MediaCard: React.FC<VideoListProps> = ({data}) => {
   // console.log('data :>> ', data);
   return (
     <div>
-      <Link href={{
+      {/* <Link href={{
         pathname: `videos/${data.title}`,
         query: {
           video: {
-            _id: data._id,
+            // _id: data._id,
             title: data.title,
             description: data.description,
             duration: data.duration,
             thumbnail_url: data.thumbnail_url,
             tags: data.tags
           }
-        }}}>
+        }}}> */}
         <h1>{data.title}</h1>
         <Image src={data.thumbnail_url} alt={data.title} width={100} height={100}/>
-      </Link>
+      {/* </Link> */}
     </div>
   )
 }
