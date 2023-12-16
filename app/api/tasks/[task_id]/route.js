88cd7424a1ee63b620c7f8ed2c18d0c6b57b5fa9
awe <aweful@awews.com>
2023-12-16 16:26:53 -0700

    Add get and delete task by id routes

--- app/api/tasks/[task_id]/route.js
+++ app/api/tasks/[task_id]/route.js
@@ -1,6 +1,33 @@
 import Task from "@/app/(models)/Task";
 import { NextResponse } from "next/server";
 
+export async function GET(req, {params}) {
+  try {
+
+    const { task_id } = params;
+
+    console.log('task_id :>> ', task_id);
+
+    const task = await Task.findById(task_id);
+
+    console.log('task :>> ', task);
+    return NextResponse.json({
+      task: task },{
+        message: "Task found"
+      },{
+        status: 201
+      }
+      );
+  } catch (err) {
+    console.log('POST task err :>> ', err);
+    return NextResponse.json({
+      message: `Error Post task ${err.message}`
+    },{
+      status: 500,
+    })
+  }
+}
+
 export async function PATCH(req, {params}) {
   try {
 
@@ -21,6 +48,36 @@ export async function PATCH(req, {params}) {
     return NextResponse.json({
       updatedTask: updatedTask },{
         message: "Task Updated"
+      },{
+        status: 201
+      }
+      );
+  } catch (err) {
+    console.log('POST task err :>> ', err);
+    return NextResponse.json({
+      message: `Error Post task ${err.message}`
+    },{
+      status: 500,
+    })
+  }
+}
+
+export async function DELETE(req, {params}) {
+  try {
+
+    const { task_id } = params;
+
+    console.log('task_id :>> ', task_id);
+
+    const deletedTask = await Task.findByIdAndDelete(task_id, {
+      new: true, // Return the new document version not the old one.
+      runValidators: true,
+    });
+
+    console.log('deletedTask :>> ', deletedTask);
+    return NextResponse.json({
+      deletedTask: deletedTask },{
+        message: "Task Deleted"
       },{
         status: 201
       }
