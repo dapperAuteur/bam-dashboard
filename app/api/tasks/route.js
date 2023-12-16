4fc389dfba23b7fa60434e95de01e4eb242c2246
awe <aweful@awews.com>
2023-12-16 15:49:24 -0700

    create route to get all tasks and create one task

---
+++ app/api/tasks/route.js
@@ -0,0 +1,66 @@
+import Task from "../../(models)/Task";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  try {
+    const body = await req.json();
+    const taskData = body.taskData;
+    console.log('body :>> ', body);
+    console.log('taskData :>> ', taskData);
+
+    if (!taskData?.task_name) {
+      return NextResponse.json({
+        message: "task_name is required."
+      },{
+        status: 400
+      });
+    }
+
+    const duplicate = await Task.findOne({ task_name: taskData?.task_name }).
+      lean()
+      .exec();
+
+    if (duplicate) {
+      return NextResponse.json({
+        message: "Duplicate Task"
+      },
+      {
+        status: 409
+      })
+    }
+
+    await Task.create(taskData);
+    return NextResponse.json({
+      message: "Task created."
+    },{
+      status: 201
+    })
+  } catch (err) {
+    console.log('POST task err :>> ', err);
+    return NextResponse.json({
+      message: `Error Post task ${err.message}`
+    },{
+      status: 500,
+    })
+  }
+}
+
+export async function GET() {
+  try {
+    const tasks = await Task.find();
+    console.log('tasks :>> ', tasks);
+    
+    return NextResponse.json({
+      tasks: tasks
+    },{
+      status: 200
+    })
+  } catch (err) {
+    console.log('GET tasks err :>> ', err);
+    return NextResponse.json({
+      error: 'An error occurred while fetching the tasks'
+    },{
+      status: 500
+    })
+  }
+}
